c9356c339b9131f12006354a9d618c912177d86e
Oscar Viciana <oviciana@gmail.com>
2015-12-02 17:38:33 +0100

    -

--- resources/views/headerData.blade.php
+++ resources/views/headerData.blade.php
@@ -1,11 +1,11 @@
 @extends('indexplantilla')
 
-<!--Añadir metas, js, css y favicon.-->
 @section('headerData')
+    {Añadir metas, js, css y favicon.}
     <meta name="application-name" content="GE"/>
     <meta name="description" content="Visionado de notas con PHP(Laravel), MySQL, ImageMagick."/>
     <meta name="author" content="Oscar Viciana y David Postigo">
-    <link rel="icon" href="{{ URL::asset('favicon.ico') }}">
+    <link rel="icon" href="{{ URL::asset('/img/favicon.ico') }}">
     <link rel="stylesheet" href="{{ URL::asset('/CSS/style.css') }}" type="text/css">
 @endsection
 
